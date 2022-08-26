7c0de77af0ce9b027961fed9c79260d5a3c52981
Kalmarv <59708937+Kalmarv@users.noreply.github.com>
2022-08-25 20:32:21 -0600

    🚧 Why are refs so complicated

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+/* eslint-disable react/display-name */
 /* eslint-disable react/prop-types */
 import { Environment, OrbitControls, PerspectiveCamera, Trail } from '@react-three/drei'
 import { Canvas, ReactThreeFiber, useFrame, useThree } from '@react-three/fiber'
 import { Debug, Physics, RigidBody, RigidBodyApi } from '@react-three/rapier'
 import { useControls } from 'leva'
-import { Suspense, useEffect, useRef, useState } from 'react'
+import React, { forwardRef, Suspense, useEffect, useRef, useState } from 'react'
 import * as THREE from 'three'
 
 const randomRange = (min: number, max: number) => Math.random() * (max - min) + min
@@ -30,68 +31,63 @@ const Ball = ({ ...props }) => {
   )
 }
 
-const MouseIdentifier = () => {
-  const meshRef = useRef<THREE.Mesh>(null)
-  const floorRef = useRef<THREE.Mesh>(null)
+type useMouse = {
+  intersectionObject: THREE.Mesh | null
+  displayObject: THREE.Mesh | null
+}
+
+const useMouse = (params: useMouse) => {
+  const { intersectionObject, displayObject } = params
   const { camera, raycaster } = useThree()
 
   useFrame(({ mouse }) => {
     const mousePosition = new THREE.Vector3(mouse.x, mouse.y, 0)
 
-    if (meshRef.current && floorRef.current) {
+    if (intersectionObject && displayObject) {
       raycaster.setFromCamera(mousePosition, camera)
-      const intersections = raycaster.intersectObject(floorRef.current)
+      const intersections = raycaster.intersectObject(intersectionObject)
       const intersectionPoint = intersections?.[0]
-        ? [intersections[0].point.x, intersections[0].point.y + 1, intersections[0].point.z]
+        ? [intersections[0].point.x, intersections[0].point.y + 0.5, intersections[0].point.z]
         : [0, 0, 0]
 
       const [x, y, z] = intersectionPoint
 
-      meshRef.current.position.set(x, y, z)
+      displayObject.position.set(x, y, z)
     }
   })
-
-  return (
-    <>
-      <mesh ref={meshRef}>
-        <sphereGeometry />
-        <meshStandardMaterial color={'red'} />
-      </mesh>
-      <mesh ref={floorRef}>
-        <boxGeometry args={[50, 1, 50]} />
-        <meshStandardMaterial visible={false} />
-      </mesh>
-    </>
-  )
 }
 
 const Cue = ({ ...props }) => {
   const [isHovered, setIsHovered] = useState(false)
   const [isSelected, setIsSelected] = useState(false)
   const [cuePosition, setCuePosition] = useState(new THREE.Vector3(0, 0, 0))
-  const cueRef = useRef<RigidBodyApi>(null!)
-  const meshRef = useRef<THREE.Mesh>(null!)
+  const cueRef = useRef<RigidBodyApi>(null)
+  const cueMeshRef = useRef<THREE.Mesh>(null)
+  const floorRef = useRef<THREE.Mesh>(null)
+  const mousePointRef = useRef<THREE.Mesh>(null)
 
   // useFrame((state, delta) => {
   //   console.log(meshRef.current.getWorldPosition(cueRef.current.translation()))
   // })
 
   useEffect(() => {
-    if (cueRef.current && isSelected) {
+    if (cueRef.current && isSelected && cueMeshRef.current) {
       // cueRef.current.applyImpulse({ x: 0, y: 0, z: 10 })
       cueRef.current.setLinvel({ x: 0, y: 0, z: 0 })
       cueRef.current.setAngvel({ x: 0, y: 0, z: 0 })
-      setCuePosition(meshRef.current.getWorldPosition(cueRef.current.translation()))
-      // console.log(cueRef.current.translation())
+      setCuePosition(cueMeshRef.current.getWorldPosition(cueRef.current.translation()))
     }
   }, [cueRef, isSelected])
 
+  useMouse({ intersectionObject: floorRef.current, displayObject: mousePointRef.current })
+
   return (
     <>
+      {/* {cueMeshRef.current && <MouseIdentifier intersectionObject={cueMeshRef.current} />} */}
       <RigidBody colliders='ball' type='dynamic' ref={cueRef}>
         <mesh
           {...props}
-          ref={meshRef}
+          ref={cueMeshRef}
           onPointerOver={() => setIsHovered((hovered) => !hovered)}
           onPointerOut={() => setIsHovered((hovered) => !hovered)}
           onPointerDown={() => setIsSelected((selected) => !selected)}
@@ -100,6 +96,14 @@ const Cue = ({ ...props }) => {
           <meshStandardMaterial color={isHovered ? 'hotpink' : 'white'} />
         </mesh>
       </RigidBody>
+      <mesh ref={floorRef}>
+        <boxGeometry args={[50, 1, 50]} />
+        <meshStandardMaterial visible={false} />
+      </mesh>
+      <mesh ref={mousePointRef}>
+        <sphereGeometry />
+        <meshStandardMaterial color={'red'} />
+      </mesh>
     </>
   )
 }
@@ -109,7 +113,6 @@ const Stage = () => {
   return (
     <Physics colliders={false}>
       {debug && <Debug />}
-      <MouseIdentifier />
       <Cue position={[0, 5, 0]} />
       <Ball position={[randomRange(-5, 5), 10, randomRange(-5, 5)]} />
       <Ball position={[randomRange(-5, 5), 10, randomRange(-5, 5)]} />
